dec75d66937e2f563480de315478ded570f11321
1jamflyer <1jamflyer@gmail.com>
2017-10-20 15:41:33 -0500

    lab4
    exercise 1&2

---
+++ maze.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function(){
+ var start =  $("#start");
+ var end = $("#end");
+ var maze = $("#maze");
+ var boundary = $(".boundary");
+ var gameStart = false;
+ maze.mouseleave(function(){ loss() });
+
+
+
+})
