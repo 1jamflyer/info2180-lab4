962d066f073c8bb7fa5026a79a3a36a3d85ee92a
1jamflyer <1jamflyer@gmail.com>
2017-10-20 15:44:13 -0500

    lab4
    exercise 3-5

--- maze.js
+++ maze.js
@@ -8,6 +8,31 @@ $(document).ready(function(){
  var gameStart = false;
  maze.mouseleave(function(){ loss() });
 
+ start.mouseover(function(){
+  $("#status").text('Move mouse to end to win the game');
+  gameStart = true;
+  if(boundary.hasClass('youlose')){
+   boundary.removeClass('youlose');
+  }
+  boundary.mousemove(function(){ loss(); }) 
+ });
 
+ end.mousemove(function(){
+  if(gameStart == true) won(); 
+  else if(gameStart && boundary.hasClass('youlose'))  loss(); 
+ 
+ });
 
+ function won(){
+  gameStart = false;
+  $("#status").text("Congrats you won (^_^)");
+ }
+
+ function loss(){
+  if(gameStart){
+   gameStart = false;
+   boundary.addClass('youlose');
+   $("#status").text('GG you lost, Try next time');
+  }
+ }
 })
